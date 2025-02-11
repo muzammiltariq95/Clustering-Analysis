3722c84ca717db44863c8712fc892d1dd78d19e7
MuzammilTariq <muzammiltariq95@gmail.com>
2025-02-11 01:02:18 +0000

    Importing libraries and dataset

---
+++ Clustering.py
@@ -0,0 +1,27 @@
+# !pip install numpy pandas seaborn matplotlib scikit-learn 
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn import preprocessing
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from sklearn import metrics
+from sklearn.metrics import silhouette_score
+from sklearn.neighbors import NearestNeighbors
+from sklearn.cluster import DBSCAN
+
+
+# %matplotlib inline
+
+import os
+os.environ['OMP_NUM_THREADS'] = '1'
+
+dataset = pd.read_csv('hflights.csv')
+
+dataset.shape
